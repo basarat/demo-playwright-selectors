916e1c9136bbd5d4416617af004cc9acfe823706
basarat <basaratali@gmail.com>
2022-03-08 15:03:35 +1100

    better

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next';
 const Home: NextPage = () => {
   return (
     <div style={{ padding: '10px' }}>
-      <button className='primary' type='submit'>Submit</button> <button className='secondary' type='reset'>Reset</button>
+      <button data-test="submit" className='primary' type='submit'>Submit</button> <button className='secondary' type='reset'>Reset</button>
     </div>
   );
 }
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,5 +4,22 @@ test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:3000');
 });
 
-test('selectors', () => {
+test('selectors', async ({ page }) => {
+  /** CSS Selector */
+  await page.locator('button.primary').click();
+
+  /** Explicit CSS Selector */
+  await page.locator('css=button.primary').click();
+
+  /** Text Selector */
+  await page.locator('text=Submit').click();
+
+  /** Chain Selectors */
+  await page.locator('button >> text=Submit').click();
+
+  /** nth */
+  await page.locator('button >> nth=0').click();
+
+  /** data-test */
+  await page.locator('data-test=submit').click();
 });
