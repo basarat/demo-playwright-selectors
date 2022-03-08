738ff8c1af0dd5b5b61bbddd50e68f585dd43f6a
basarat <basaratali@gmail.com>
2022-03-08 14:58:17 +1100

    simpler

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,7 @@ import type { NextPage } from 'next';
 const Home: NextPage = () => {
   return (
     <div style={{ padding: '10px' }}>
-      <button className='primary' type='submit'>Submit</button>
-      <br />
-      <br />
-      <button className='secondary' type='reset'>Reset</button>
+      <button className='primary' type='submit'>Submit</button> <button className='secondary' type='reset'>Reset</button>
     </div>
   );
 }
